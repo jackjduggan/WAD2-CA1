fd312fe16f3cafb4d4f5c2ac02e54b77199af0d0
Jack Duggan <jackduggan01@hotmail.com>
2022-10-17 12:19:30 +0100

    Movie Details Page - included production countries.

--- src/stories/sampleData.js
+++ src/stories/sampleData.js
@@ -61,6 +61,11 @@ export default {
         iso_3166_1: "US",
         name: "United States of America",
       },
+      {
+        iso_3166_1: "IE",
+        name: "Ireland",
+
+      },
     ],
     release_date: "2017-12-13",
     revenue: 1332459537,
