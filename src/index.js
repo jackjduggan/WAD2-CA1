27c63aa0a81352784c951753541112ef1d56b43f
Jack Duggan <jackduggan01@hotmail.com>
2022-12-01 14:59:25 +0000

    index page updated for tv shows - not tested

--- src/index.js
+++ src/index.js
@@ -1,17 +1,27 @@
 import React from "react";
 import {createRoot} from "react-dom/client";
 import { BrowserRouter, Route, Navigate, Routes } from "react-router-dom";
+//Home Pages
 import HomePage from "./pages/homePage";
 import MoviePage from "./pages/movieDetailsPage";
+import ShowsPage from "./pages/showsHomePage";
+// Other Pages
 import UpcomingMoviesPage from "./pages/upcomingMoviesPage";
-import FavouriteMoviesPage from "./pages/favouriteMoviesPage"; // NEW
-//import {Link} from 'react-router-dom'
+import FavouriteMoviesPage from "./pages/favouriteMoviesPage";
+import FavouriteShowsPage from "./pages/favouriteShowsPage";
+// Review Pages
 import MovieReviewPage from "./pages/movieReviewPage";
+//import AddMovieReviewPage from './pages/addMovieReviewPage';
+import WriteReviewPage from "./pages/addMovieReviewPage";
+import ShowReviewPage from "./pages/showReviewPage";
+import WriteShowReviewPage from "./pages/addShowReviewPage";
+// Other
 import SiteHeader from './components/siteHeader';
 import { QueryClientProvider, QueryClient } from "react-query";
 import { ReactQueryDevtools } from 'react-query/devtools';
+// Context Providers
 import MoviesContextProvider from "./contexts/moviesContext";
-import AddMovieReviewPage from './pages/addMovieReviewPage';
+import ShowsContextProvider from "./contexts/ShowsContext";
 
 const queryClient = new QueryClient({
   defaultOptions: {
@@ -31,7 +41,7 @@ const App = () => {
           <SiteHeader />      {/* New Header  */}
           <MoviesContextProvider>
             <Routes>
-              <Route path="/reviews/form" element={<AddMovieReviewPage/>} />
+              <Route path="/reviews/form" element={<WriteReviewPage/>} />
               <Route path="/reviews/:id" element={ <MovieReviewPage /> } />
               <Route exact path="/movies/favourites" element={<FavouriteMoviesPage/>} />
               <Route exact path="/movies/upcoming" element={<UpcomingMoviesPage/>} />
@@ -40,6 +50,16 @@ const App = () => {
               <Route path="*" element={ <Navigate to="/" /> } />
             </Routes>
           </MoviesContextProvider>
+          <ShowsContextProvider>
+            <Routes>
+              <Route path="/showreviews/form" element={<WriteShowReviewPage/>} />
+              <Route path="/showreviews/:id" element={ <ShowReviewPage /> } />
+              <Route exact path="/shows/favourites" element={<FavouriteShowsPage/>} />
+              <Route path="/shows/:id" element={<ShowsPage />} />
+              <Route path="/" element={<HomePage />} />
+              <Route path="*" element={ <Navigate to="/" /> } />
+            </Routes>
+          </ShowsContextProvider>
         </BrowserRouter>
         <ReactQueryDevtools initialIsOpen={false} />
       </QueryClientProvider>
