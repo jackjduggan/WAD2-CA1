3d003ea07a282207fd2932b8347d531867c7f643
Jack Duggan <jackduggan01@hotmail.com>
2022-12-05 09:56:10 +0000

    Updated Site Title

--- src/components/siteHeader/index.js
+++ src/components/siteHeader/index.js
@@ -44,10 +44,10 @@ const SiteHeader = ({ history }) => {
       <AppBar position="fixed" color="secondary">
         <Toolbar>
           <Typography variant="h4" sx={{ flexGrow: 1 }}>
-            TMDB Client
+            JD Entertainment
           </Typography>
           <Typography variant="h6" sx={{ flexGrow: 1 }}>
-            All you ever wanted to know about Movies!
+            One Stop Shop For Everything TV
           </Typography>
             {isMobile ? (
               <>
